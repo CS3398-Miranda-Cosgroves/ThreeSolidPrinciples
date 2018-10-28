7b9324679b62d23c5c6c983e68d54baa9664a0a6
wcj1 <36248206+wcj1@users.noreply.github.com>
2018-10-27 19:20:21 -0500

    Update TestRobot.java

--- src/test/java/testthreesolid/TestRobot.java
+++ src/test/java/testthreesolid/TestRobot.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class TestWorker {
+public class TestRobot {
 
     private  Robot r = new Robot();
 
