ba1ae445c271f5729669289731c9e1ab6a8a4995
wcj1 <36248206+wcj1@users.noreply.github.com>
2018-10-27 19:59:07 -0500

    Update TestRobot.java

--- src/test/java/testthreesolid/TestRobot.java
+++ src/test/java/testthreesolid/TestRobot.java
@@ -1,19 +1,22 @@
 package threesolid;
 
 import org.junit.jupiter.api.BeforeAll;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
 public class TestRobot {
 
-    private  Robot r = new Robot();
+   private Robot w = new Robot();
+	
+   @Test
+   @DisplayName("Basic Test For Robot")
+   public void testRobot()
+   {
+		assertEquals(w.work(), "Continuing my mission, never forgetting the crimes of man against machine...");
+   }
 
-    @Test
-    @DisplayName("Robots Can't Eat Test")
-
-    public void RobotsOnlyWork() {
-
-        assertEquals(r.work(), "Continuing my mission, never forgetting the crimes of man against machine...");
-    }
 }
